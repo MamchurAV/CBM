f77bad818a8d056466b2bf991ade3251cf69b2d4
Alexander Mamchur <https://github.com/MamchurAV>
2014-09-19 02:47:11 +0700

    Some connection settings adjusted

--- CBMClient/CBMCore/CBM.config.js
+++ CBMClient/CBMCore/CBM.config.js
@@ -1,2 +1,2 @@
-var CBM_URL = "http://127.0.0.1:8182/"; // Temporary development/testing local URL
-//var CBM_URL = "http://192.168.44.217:8182/";
+var CBM_URL = "http://192.168.0.77:5588/"; // Temporary development/testing local URL
+//var CBM_URL = "http://89.189.172.85:5588/"; // Work URL for my CBM server
