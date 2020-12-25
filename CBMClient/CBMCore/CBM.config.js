d8da085a12193378ff693f909620f5f2c151c8e8
Alexander Mamchur <https://github.com/MamchurAV>
2020-12-25 20:26:22 +0700

    Текущее состояние

--- CBMClient/CBMCore/CBM.config.js
+++ CBMClient/CBMCore/CBM.config.js
@@ -1,6 +1,6 @@
-var CBM_URL = "http://CBModel.ru:5588/"; // Work URL for global CBM server
+//var CBM_URL = "http://CBModel.ru:5588/"; // Work URL for global CBM server
 //var CBM_URL = "http://127.0.0.1:5588/"; // Temporary development/testing local URL
-//var CBM_URL = "http://localhost:5588/"; // Work URL for my CBM server
+var CBM_URL = "http://localhost:5588/"; // Work URL for my CBM server
 
 ///////////////////////// Application settings ////////////////////
 
