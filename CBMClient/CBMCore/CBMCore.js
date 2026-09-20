fe33bcd95c62651b456ccf6075c9738884bf3b8e
AlexMAV <mamchurav@gmail.com>
2026-09-21 01:30:36 +0700

    Удалил полностью токен leaflet

--- CBMClient/CBMCore/CBMCore.js
+++ CBMClient/CBMCore/CBMCore.js
@@ -6365,8 +6365,7 @@ isc.LeafletCanvas.addProperties({
                     'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                 maxZoom: 21,
                 id: "mapbox.streets",
-                accessToken: "<Leaflet_token_here>"
-//                    //"pk.eyJ1IjoiYWxleG1hdiIsImEiOiJjajJkcWNoOGcwMDNpMndudDF0M2xpaHVpIn0.iLB4oEdNFOAZPo_PVrFdfw",
+                accessToken: "<Leaflet_token_here>" // <<< TODO - provide external token here
             },
         ).addTo(this.mymap);
 
